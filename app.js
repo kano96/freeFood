c6ec480fd36a95f8073ff27a8fa7d110ff361cb5
Kevin Torres <kevin_akd@hotmail.com>
2022-01-04 12:30:13 -0500

    first commit

---
+++ app.js
@@ -0,0 +1,28 @@
+const express = require("express");
+const cors = require("cors");
+const path = require("path");
+
+const router = require("./src/routes");
+
+const { json, urlencoded } = express;
+
+const app = express();
+
+const port = process.env.PORT || 8080;
+
+const corsOptions = {
+  origin: "*",
+  optionSuccessStatus: 200,
+};
+
+app.use(cors(corsOptions));
+
+app.use(router);
+
+app.use("/", (req, res) => {
+  res.send("First microservice");
+});
+
+app.listen(port, () => {
+  console.log(`Server listening on port ${port}`);
+});
