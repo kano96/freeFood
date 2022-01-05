bcb97f592ec34389bf1ed2d12e70acc7e8cbfcc6
Kevin Torres <kevin_akd@hotmail.com>
2022-01-05 10:40:00 -0500

    added handle buy to market

--- api/src/controllers/food.controller.js
+++ api/src/controllers/food.controller.js
@@ -2,6 +2,7 @@ const { ingredients, recipes } = require("../db");
 
 const numberOfRecipes = Object.keys(ingredients).length;
 
+//look if there are enough ingredients for recipe
 function validateIngredients(recipe) {
   const wanted = {};
   for (let ingredient in recipe) {
@@ -12,14 +13,43 @@ function validateIngredients(recipe) {
   return Object.keys(wanted).length ? wanted : true;
 }
 
+//decrease ingredients on kitchen  when recipe is done
 function decreaseIngredientsOnKitchen(recipe) {
   for (let ingredient in recipe) {
     ingredients[ingredient] -= recipe[ingredient];
   }
 }
 
-async function buyIngredients(ingredients) {}
+//buy enough ingredients in order to do the recipe
+async function buyIngredients(ingredientsNeeded) {
+  const ingredientsToBuy = Object.keys(ingredientsNeeded);
+  let ingredientsCheckList = {};
+  for (let ingredient of ingredientsToBuy) {
+    ingredientsCheckList[ingredient] = false;
+  }
+  let notEnoughIngredients = true;
+  let error = false;
 
+  while (notEnoughIngredients) {
+    for (let ingredient of ingredientsToBuy) {
+      fetch(
+        `https://recruitment.alegra.com/api/farmers-market/buy/?ingredient=${ingredient}`
+      )
+        .then((response) => response.json())
+        .then((data) => (ingredients[ingredient] += data.quantitySold))
+        .catch((e) => (error = true));
+      if (ingredients[ingredient] >= ingredientsNeeded[ingredient]) {
+        ingredientsCheckList[ingredient] = true;
+      }
+    }
+    if (Object.values(ingredientsCheckList).every((el) => el === true)) {
+      notEnoughIngredients = false;
+    }
+  }
+  return error ? "Market closed" : true;
+}
+
+//generate random number for recipe
 function selectRecipeRandom(max) {
   return `recipe${Math.floor(Math.random() * max + 1)}`;
 }
@@ -33,6 +63,12 @@ async function getFood(req, res) {
     res.json({ msg: "food delivered" });
   } else {
     const ingredientsOnMarket = await buyIngredients(ingredientsOnKitchen);
+    if (ingredientsOnMarket === true) {
+      decreaseIngredientsOnKitchen(recipeToDo);
+      res.json({ msg: "food delivered" });
+    } else {
+      res.json({ msg: "market closed" });
+    }
   }
 }
 
